eca6d64563f926052163d0853b5ac6d7be1faeb8
Dykam <kramieb@gmail.com>
2016-01-02 22:21:14 +0100

    Removed dependency on JetBrains.Annotations in preperation of dnxcore50 support

--- src/CompSharp/SupportAttribute.cs
+++ src/CompSharp/SupportAttribute.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using JetBrains.Annotations;
 
 namespace CompSharp
 {
 
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true)]
-    [MeansImplicitUse]
     public class SupportAttribute : Attribute
     {
     }
